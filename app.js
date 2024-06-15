1736dcf31fc487a786c387b0dd09a2c525e20899
Abir Bera <abirbera2003@gmail.com>
2024-06-15 19:06:10 +0530

    profile api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import connectToDB from "./db/connectToDB.js";
 import checkRoutes from "./routes/checkRoutes.js";
 import myBooksRoutes from "./routes/userRoutes/myBooks/myBooksRoutes.js";
 import booksRoutes from "./routes/userRoutes/books/booksRoutes.js";
-
+import profileRouter from "./routes/profileRoutes/profile.js";
 const app = express();
 
 const PORT = process.env.PORT;
@@ -12,12 +12,12 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use("/api/developer", checkRoutes);
-
 app.use("/api/user/myBooks", myBooksRoutes);
 app.use("/api/user/book/browse", booksRoutes);
+app.use("/api/user/profile", profileRouter);
 //user/book/browse
 
 app.listen(PORT, () => {
-    connectToDB();
-    console.log(`Server listening on port http://localhost:${PORT}`);
+  connectToDB();
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
