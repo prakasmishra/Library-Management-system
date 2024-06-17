7deaf094974c6e91465b743a516b6afbbb1f9817
Arpan1108JU <arpankoley345@gmail.com>
2024-06-17 09:58:17 +0530

    changed .env.example

--- controllers/adminControllers/NoticeControllers/issueNotice.js
+++ controllers/adminControllers/NoticeControllers/issueNotice.js
@@ -3,5 +3,6 @@ import driver from "../../../utils/neo4j-driver.js"
 import asyncHandler from "express-async-handler"
 
 export const issueNotice = asyncHandler(async(req,res) => {
+    // todo issue notice
     res.send("TODO : Issued notice successfully")
 })
