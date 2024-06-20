6e8087ee0d8ffc76755b9183bf2f2f228530df61
Abir Bera <abirbera2003@gmail.com>
2024-06-20 18:40:13 +0530

    Feat : Add Member Details -> Roll and Dept Support Added

--- controllers/userControllers/authControllers/authContollers.js
+++ controllers/userControllers/authControllers/authContollers.js
@@ -4,6 +4,8 @@ import parser from "parse-neo4j";
 export const addUserDetails = async (req, res) => {
   try {
     const {
+      roll,
+      dept,
       address,
       join_date,
       library_card_no,
@@ -15,13 +17,14 @@ export const addUserDetails = async (req, res) => {
     } = req.body;
     const library_card_string = "000";
     const status = "pending";
-    const q=`
+    const q = `
     MATCH (m:Member {membership_id: $membership_id}) 
     RETURN m.membership_id as id`;
-    const r=  await driver.executeQuery(q, {membership_id:library_card_no});
+    const r = await driver.executeQuery(q, { membership_id: library_card_no });
     const response = parser.parse(r);
     const query = `MERGE (m:Member {membership_id: $membership_id})
     SET m.library_card_string=$library_card_string,
+    m.roll = $roll,
         m.address = $address,
         m.join_date = $join_date,
         m.first_name = $first_name,
@@ -33,6 +36,7 @@ export const addUserDetails = async (req, res) => {
     RETURN m`;
 
     const context = {
+      roll: roll,
       membership_id: library_card_no,
       first_name: first_name,
       last_name: last_name,
@@ -46,12 +50,19 @@ export const addUserDetails = async (req, res) => {
     };
     console.log("hello");
     const result = await driver.executeQuery(query, context);
-    // console.log(result);
-    console.log(result.records[0].get("m").properties);
-    if(response.length==0){
+    const member = parser.parse(result);
+    console.log(result);
+    console.log();
+    if (response.length == 0) {
+      const deptQuery = `MATCH (m:Member {membership_id: $membership_id}), 
+                        (d:Department {dept_id : $dept})
+                        CREATE (m)-[:ENROLLED_IN]->(d)
+                        return m,d`;
+      const deptContext = { membership_id: library_card_no, dept: dept };
+      const deptRes = await driver.executeQuery(deptQuery, deptContext);
+      console.log("Success");
       res.send({ message: "Details added Successfully" }).status(200);
-    }
-    else{
+    } else {
       res.send({ message: "Details updated Successfully" }).status(200);
     }
   } catch (error) {
